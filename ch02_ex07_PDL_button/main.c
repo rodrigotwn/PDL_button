8ccb073161a78eac8ae0c9fd5b8b528fbad04071
rodrigotwn <110451045+rodrigotwn@users.noreply.github.com>
2022-10-22 18:51:39 -0300

    ejercicio_8

---
+++ ch02_ex07_PDL_button/main.c
@@ -0,0 +1,77 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for the Empty PSoC6 Application
+*              for ModusToolbox.
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* (c) 2019-2021, Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software ("EULA").
+*
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress's integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cy_pdl.h"
+#include "cyhal.h"
+#include "cybsp.h"
+
+
+int main(void)
+{
+    cy_rslt_t result;
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init() ;
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    __enable_irq();
+
+    Cy_GPIO_Pin_FastInit (P13_7_PORT, P13_7_NUM, CY_GPIO_DM_STRONG, 1UL, P13_7_GPIO);
+
+    Cy_GPIO_Pin_FastInit (P0_4_PORT, P0_4_NUM, CY_GPIO_DM_PULLUP, 1UL, P0_4_GPIO);
+
+    for (;;)
+    {
+
+    if(1UL == Cy_GPIO_Read(P0_4_PORT, P0_4_NUM))
+   	{
+   	 Cy_GPIO_Write(P13_7_PORT, P13_7_NUM, 1);
+   	}
+   	else
+   	 Cy_GPIO_Write(P13_7_PORT, P13_7_NUM, 0);
+
+    }
+}
+
+/* [] END OF FILE */
